9f5bab337313421ca1b360ef91beb9407282a323
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-06-22 16:42:07 -0300

    config seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(email: 'teste@teste.com', password: 'testeteste')
+Admin.create(email: 'admin@admin.com', password: 'testeadmin')
 puts 'Admin created!'
