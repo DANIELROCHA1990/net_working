449723125b0b2f66a7c5a155043ecaa8e044a1f5
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-06-23 09:36:11 -0300

    Config test

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   it 'admin is valid' do
-    admin = Admin.new(email: 'admin@admin.com', password: 'admin123')
+    admin = Admin.new(email: 'admin@admin.com', password: 'admin123', name: 'admin1')
 
     expect(admin.email).to eql('admin@admin.com')
     expect(admin).to be_valid
   end
 
   it 'admin is not valid' do
-    admin = Admin.new(email: 'admin@admin.com')
+    admin = Admin.new(email: 'admin@admin.com', password: nil, name: nil)
 
     expect(admin).to_not be_valid
   end
