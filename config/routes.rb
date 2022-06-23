59214024927e8ed2c1be031b95f9a068ea881097
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-06-23 15:50:02 -0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # nao usa os registros
   devise_for :admins, skip: [:registrations], controllers: { sessions: 'admins/sessions' }
-  root to: 'admin/home#index'
+
   namespace :admin do
     root to: 'home#index'
     resources :admins
+  end
+
+  root to: 'user/timeline#index'
+  namespace :user do
+    # resources :users
   end
 end
