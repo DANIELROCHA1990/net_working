06739c4ead81c9cb2b83742b6128d25cdab3af13
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-06-23 11:45:00 -0300

    fix some missconfig

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # nao usa os registros
   devise_for :admins, skip: [:registrations], controllers: { sessions: 'admins/sessions' }
-
+  root to: 'admin/home#index'
   namespace :admin do
     root to: 'home#index'
     resources :admins
